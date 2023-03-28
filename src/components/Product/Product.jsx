5a2bccec3d1d40b05fb484e4b0eae3ec94560d1a
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-03-29 00:58:40 +0600

    single page complete

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
@@ -19,6 +21,7 @@ const Product = (props) => {
         className="btn-cart"
       >
         Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
       </button>
     </div>
   );
